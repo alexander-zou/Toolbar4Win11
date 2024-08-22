d02aad3acd551ba29a131dc1a5d0704364bb85bf
ALEC-LAPTOP-202\zjc <alexander.here@gmail.com>
2024-08-22 15:57:19 +0800

    +feat: add Refresh button

--- tb4w11.py
+++ tb4w11.py
@@ -24,7 +24,14 @@ def on_open_folder_clicked( stray_icon, item):
     elif sys.platform.startswith == 'linux':
         subprocess.call(('xdg-open', working_folder))
 
+def on_refresh_clicked( stray_icon, item):
+    global keep_running
+    keep_running = True
+    stray_icon.stop()
+
 def on_exit_clicked( stray_icon, item):
+    global keep_running
+    keep_running = False
     stray_icon.stop()
 
 def on_item_clicked( stray_icon, item, fullpath):
@@ -47,32 +54,37 @@ logo = Image.open( logo_path)
 working_folder = 'folder'
 if len( sys.argv) > 1:
     working_folder = sys.argv[ 1]
-item_list = []
-def travel_menuitems( fullpath):
-    if os.path.isfile( fullpath):
-        name = os.path.basename( fullpath)
-        return pystray.MenuItem( name, partial( on_item_clicked, fullpath=fullpath))
-    elif os.path.isdir( fullpath):
-        name = os.path.basename( fullpath)
-        subitem_list = []
-        for f in os.listdir( fullpath):
-            subitem_list.append( travel_menuitems( os.path.join( fullpath, f)))
-        return pystray.MenuItem( name, pystray.Menu( *subitem_list))
-    return None
-try:
-    for f in os.listdir( working_folder):
-        item_list.append( travel_menuitems( os.path.join( working_folder, f)))
-except Exception as e:
-    pymsgbox.alert( f'Failed scaning folder "{working_folder}": {e}', 'ERROR')
-    exit( 1)
 
-item_list.append( pystray.Menu.SEPARATOR)
-item_list.append( pystray.MenuItem( 'Open Folder ...', on_open_folder_clicked))
-item_list.append( pystray.MenuItem( 'Exit', on_exit_clicked))
+keep_running = True
+while keep_running:
+    item_list = []
+    def travel_menuitems( fullpath):
+        if os.path.isfile( fullpath):
+            name = os.path.basename( fullpath)
+            return pystray.MenuItem( name, partial( on_item_clicked, fullpath=fullpath))
+        elif os.path.isdir( fullpath):
+            name = os.path.basename( fullpath)
+            subitem_list = []
+            for f in os.listdir( fullpath):
+                subitem_list.append( travel_menuitems( os.path.join( fullpath, f)))
+            return pystray.MenuItem( name, pystray.Menu( *subitem_list))
+        return None
+    try:
+        for f in os.listdir( working_folder):
+            item_list.append( travel_menuitems( os.path.join( working_folder, f)))
+    except Exception as e:
+        pymsgbox.alert( f'Failed scaning folder "{working_folder}": {e}', 'ERROR')
+        exit( 1)
 
-menu = pystray.Menu( *item_list)
-stray_icon = pystray.Icon( 'Toolbar4W11', icon=logo, menu=menu)
-stray_icon.run()
+    item_list.append( pystray.Menu.SEPARATOR)
+    item_list.append( pystray.MenuItem( 'Open Folder ...', on_open_folder_clicked))
+    item_list.append( pystray.MenuItem( 'Refresh', on_refresh_clicked))
+    item_list.append( pystray.Menu.SEPARATOR)
+    item_list.append( pystray.MenuItem( 'Exit', on_exit_clicked))
+
+    menu = pystray.Menu( *item_list)
+    stray_icon = pystray.Icon( 'Toolbar4W11', icon=logo, menu=menu)
+    stray_icon.run()
 
 # End of 'tb4w11.py' 
 
